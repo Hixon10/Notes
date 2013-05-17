661052739ba7cb0c5472094c2961211d8847285a
Hixon10 <Hixon10@yandex.ru>
2013-05-17 14:31:09 +0700

    initialization of the project

---
+++ Notes.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace Notes.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            //ninjectKernel.Bind<Bd_course_work.Domain.Abstract.IFootballTeamRepository>().To<EFFootballTeamRepository>();
+            //ninjectKernel.Bind<Bd_course_work.Domain.Abstract.IFootbalGameRepository>().To<EFFootbalGameRepository>().InSingletonScope();
+            //ninjectKernel.Bind<UnituOfWork>().To<UnituOfWork>().InSingletonScope();
+        }
+    }
+}
