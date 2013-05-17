d9fc84048ab9f100c21cb8822a036e1b9cd3257c
Hixon10 <Hixon10@yandex.ru>
2013-05-17 21:43:37 +0700

    create LoginController

---
+++ Notes.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Notes.Domain.DAL;
+
+namespace Notes.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly UnituOfWork unitOfWork;
+
+        public LoginController(UnituOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
