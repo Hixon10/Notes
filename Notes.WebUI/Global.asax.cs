f3c7fe250ee114b86807b8540eeb158e002b518e
Hixon10 <Hixon10@yandex.ru>
2013-05-17 22:03:33 +0700

    Add bootstrap

--- Notes.WebUI/Global.asax.cs
+++ Notes.WebUI/Global.asax.cs
@@ -25,7 +25,7 @@ namespace Notes.WebUI
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Node", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
