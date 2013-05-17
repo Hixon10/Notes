7b4507c9bc0fbe6e7de2b6772c60675e28a2e1d7
Hixon10 <Hixon10@yandex.ru>
2013-05-17 15:11:01 +0700

    Add DAL

---
+++ Notes.Domain/DAL/EFDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using Notes.Domain.Entities;
+
+namespace Notes.Domain.DAL
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("name=EFDbContext") { }
+
+        public DbSet<Note> Note { get; set; }
+        public DbSet<NoteStatus> NoteStatus { get; set; }
+        public DbSet<NoteType> NoteType { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ Notes.Domain/DAL/UnituOfWork.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Notes.Domain.Entities;
+
+namespace Notes.Domain.DAL
+{
+    public class UnituOfWork : IDisposable
+    {
+        private GenericRepository<Note> noteRepository;
+        private GenericRepository<NoteStatus> noteStatusRepository;
+        private GenericRepository<NoteType> noteTypeRepository;
+        private GenericRepository<User> userRepository;
+
+        EFDbContext context = new EFDbContext();
+
+        #region RepositoryClasses
+
+        public GenericRepository<Note> NoteRepository
+        {
+            get
+            {
+                if (this.noteRepository == null)
+                    this.noteRepository = new GenericRepository<Note>(context);
+                return noteRepository;
+            }
+        }
+
+        public GenericRepository<NoteStatus> NoteStatusRepository
+        {
+            get
+            {
+                if (this.noteStatusRepository == null)
+                    this.noteStatusRepository = new GenericRepository<NoteStatus>(context);
+                return noteStatusRepository;
+            }
+        }
+
+        public GenericRepository<NoteType> NoteTypeRepository
+        {
+            get
+            {
+                if (this.noteTypeRepository == null)
+                    this.noteTypeRepository = new GenericRepository<NoteType>(context);
+                return noteTypeRepository;
+            }
+        }
+
+        public GenericRepository<User> UserRepository
+        {
+            get
+            {
+                if (this.userRepository == null)
+                    this.userRepository = new GenericRepository<User>(context);
+                return userRepository;
+            }
+        }
+
+        #endregion
+
+        public void Save()
+        {
+            if (context != null) context.SaveChanges();
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
